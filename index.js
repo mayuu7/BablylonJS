c8866411f9f2fe4a33bb1c52f73b0ca1eaa62371
mayuu7 <jainmayank0@gmail.com>
2019-12-08 12:02:36 +0530

    modified the JS code to load the .glb file without default loader

--- index.js
+++ index.js
@@ -2,31 +2,29 @@ window.addEventListener('DOMContentLoaded', function() {
         // All the following code is entered here.
         var canvas = document.getElementById('renderCanvas');
         var engine = new BABYLON.Engine(canvas, true);
-
+        
         var createScene = function() {
 		    // Create a basic BJS Scene object.
 		   	var scene = new BABYLON.Scene(engine);
 
-		   	//Adding an Arc Rotate Camera
+		   	// Create a default skybox with an environment.
+    // var hdrTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("textures/environment.dds", scene);
+    // var currentSkybox = scene.createDefaultSkybox(hdrTexture, true);
+
+    //Adding an Arc Rotate Camera
     		var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0.8, 100, BABYLON.Vector3.Zero(), scene);
     		camera.attachControl(canvas, false);
+    // Append glTF model to scene.
+    BABYLON.SceneLoader.ShowLoadingScreen = false;
+    BABYLON.SceneLoader.Append("/", "Darwin.glb", scene, function (scene) {
+        // Create a default arc rotate camera and light.
+        scene.createDefaultCameraOrLight(true, true, true);
 
-		    var assetsManager = new BABYLON.AssetsManager(scene);
-			var meshTask = assetsManager.addMeshTask("Darwin task", "", "../", "Darwin.glb");
-			
-			meshTask.onSuccess = function (task) {
-			    task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
-			}	
-
-		    
-			
-			assetsManager.onFinish = function (tasks) {
-				engine.runRenderLoop(function () {
-					scene.render();
-				});
-			};
-			
-			assetsManager.load();
+        // The default camera looks at the back of the asset.
+        // Rotate the camera by 180 degrees to the front of the asset.
+        scene.activeCamera.alpha += Math.PI;
+    });
+    
 
 		    // Return the created scene.
 		    return scene;
@@ -34,6 +32,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
 		var scene = createScene();
 		engine.runRenderLoop(function() {
+		    
 		    scene.render();
 		});
 
